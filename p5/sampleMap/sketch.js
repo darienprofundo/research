dfd7efba5290c89a87e5b027fa225dd98a8b15dc
Kim Brucker <kkbruck2@ilstu.edu>
2021-04-20 17:36:35 -0500

    updates

--- p5/sampleMap/sketch.js
+++ p5/sampleMap/sketch.js
@@ -1,8 +1,24 @@
+//====================================variables
 let map;
 
+//====================================preload
+function preload() {
+
+}
+//====================================setup
+function setup() {
+
+
+}
+
+//===================================map elements
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
+}
+//===================================draw
+function draw() {
+
 }
