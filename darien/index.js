dbbc9230dc9663e5a25aef3d72f0a7073a1f5280
Kim Brucker <kkbruck2@ilstu.edu>
2021-04-22 14:09:46 -0500

    server rebuild

--- darien/index.js
+++ darien/index.js
@@ -34,7 +34,7 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen(3000, () => console.log(listening at 3000));
 app.use(express.static('public'));
 // app.use(express.json({limit: '1mb'}));
 //
