c60bf322e54341ed83f190d38c653ce18df0be40
Kim Brucker <kkbruck2@ilstu.edu>
2021-04-24 20:01:04 -0500

    Kobo install db

--- darien/index.js
+++ darien/index.js
@@ -1,4 +1,6 @@
+
 const express = require('express');
+
 const Datastore = require('nedb');
 const fetch = require('node-fetch');
 require('dotenv').config();
@@ -9,48 +11,17 @@ app.listen(port, () => {
   console.log(`Starting server at ${port}`);
 });
 app.use(express.static('public'));
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({
+  limit: '1mb'
+}));
 
-const database = new Datastore('database.db');
-database.loadDatabase();
-
-app.get('/api', (request, response) => {
-  database.find({}, (err, data) => {
-    if (err) {
-      response.end();
-      return;
-    }
-    response.json(data);
-  });
-});
-
-app.post('/api', (request, response) => {
-  const data = request.body;
-  const timestamp = Date.now();
-  data.timestamp = timestamp;
-  database.insert(data);
-  response.json(data);
-});
-
-app.get('/weather/:latlon', async (request, response) => {
+app.get('/toolbox', async (request, response) => {
   console.log(request.params);
-  const latlon = request.params.latlon.split(',');
-  console.log(latlon);
-  const lat = latlon[0];
-  const lon = latlon[1];
-  console.log(lat, lon);
-  const api_key = process.env.API_KEY;
-  const weather_url = `https://api.darksky.net/forecast/${api_key}/${lat},${lon}/?units=si`;
-  const weather_response = await fetch(weather_url);
-  const weather_data = await weather_response.json();
+  const kobo_url = 'https:kc.kobotoolbox.org/api/v1/data.json?Authorization:Token2c773a1c48ccd8af2f3ec3213441df8ea023a9b8';
+  const kobo_response = await fetch(kobo_url);
+  const kobo_data = await kobo_response.json();
 
-  const aq_url = `https://api.openaq.org/v1/latest?coordinates=${lat},${lon}`;
-  const aq_response = await fetch(aq_url);
-  const aq_data = await aq_response.json();
-
-  const data = {
-    weather: weather_data,
-    air_quality: aq_data
-  };
   response.json(data);
+  console.log(data);
+
 });
