25b81e88662f97f82e1b2ee527f243a60d3994fb
Kim Brucker <kkbruck2@ilstu.edu>
2022-06-26 14:00:33 -0500

    062522update

--- APIs/index.js
+++ APIs/index.js
@@ -2,3 +2,4 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
